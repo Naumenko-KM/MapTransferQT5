fae7b66ba4ed9ca5f445bae633632498e9ee015f
Naumenko-KM <Naumenko-KM@yandex.ru>
2022-06-08 15:22:18 +0300

    Add images on main window

--- MainWindow.py
+++ MainWindow.py
@@ -12,71 +12,89 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 524)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+
+        self.setObjectName("MainWindow")
+        self.resize(800, 524)
+        self.img_size = 512
+        self.centralwidget = QtWidgets.QWidget()
         self.centralwidget.setObjectName("centralwidget")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(660, 60, 89, 25))
         self.pushButton.setObjectName("pushButton")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(40, 60, 281, 17))
-        self.label.setObjectName("label")
+        self.pushButton.setText("New image")
+
         self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.layoutWidget.setGeometry(QtCore.QRect(40, 110, 731, 341))
         self.layoutWidget.setObjectName("layoutWidget")
+        self.pushButton_2 = QtWidgets.QPushButton(self.layoutWidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_2.setText("Quit")
+        self.pushButton_3 = QtWidgets.QPushButton(self.layoutWidget)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_3.setText("Save")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 60, 281, 17))
+        self.label.setObjectName("label")
+        self.label.setText("Filename: Example_1.png")
+
+
         self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.graphicsView_2 = QtWidgets.QGraphicsView(self.layoutWidget)
-        self.graphicsView_2.setObjectName("graphicsView_2")
-        self.horizontalLayout.addWidget(self.graphicsView_2)
-        self.graphicsView = QtWidgets.QGraphicsView(self.layoutWidget)
-        self.graphicsView.setObjectName("graphicsView")
-        self.horizontalLayout.addWidget(self.graphicsView)
+
+        file_name_0 = '20.0_49.0_vis.jpeg'
+        image_0  = QtGui.QPixmap(file_name_0)
+        image_0 = image_0.scaled(self.img_size, self.img_size, QtCore.Qt.KeepAspectRatio)
+        self.label_img_0 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_img_0.setObjectName("label_image_0")
+        self.label_img_0.setPixmap(image_0)
+        self.label_img_0.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.horizontalLayout.addWidget(self.label_img_0)
+
+        file_name_1 = '20.0_49.0_inf.jpeg'
+        image_1  = QtGui.QPixmap(file_name_1)
+        image_1 = image_1.scaled(self.img_size, self.img_size, QtCore.Qt.KeepAspectRatio)
+        self.label_img_1 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_img_1.setObjectName("label_image_1")
+        self.label_img_1.setPixmap(image_1)
+        self.label_img_1.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.horizontalLayout.addWidget(self.label_img_1)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setSpacing(500)
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.pushButton_2 = QtWidgets.QPushButton(self.layoutWidget)
-        self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout_2.addWidget(self.pushButton_2)
-        self.pushButton_3 = QtWidgets.QPushButton(self.layoutWidget)
-        self.pushButton_3.setObjectName("pushButton_3")
         self.horizontalLayout_2.addWidget(self.pushButton_3)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.setCentralWidget(self.centralwidget)
+
+        self.menuBar = QtWidgets.QMenuBar()
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 800, 22))
         self.menuBar.setObjectName("menuBar")
         self.menuCreate = QtWidgets.QMenu(self.menuBar)
         self.menuCreate.setObjectName("menuCreate")
-        MainWindow.setMenuBar(self.menuBar)
-        self.menuCreate.addSeparator()
-        self.menuCreate.addSeparator()
+        self.menuCreate.setTitle("Change mode")
+        self.setMenuBar(self.menuBar)
         self.menuCreate.addSeparator()
         self.menuBar.addAction(self.menuCreate.menuAction())
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(self)
+        
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "New image"))
-        self.label.setText(_translate("MainWindow", "Filename: Example_1.png"))
-        self.pushButton_2.setText(_translate("MainWindow", "Quit"))
-        self.pushButton_3.setText(_translate("MainWindow", "Save"))
-        self.menuCreate.setTitle(_translate("MainWindow", "Create"))
 
 app = QtWidgets.QApplication(sys.argv)
-window = QtWidgets.QMainWindow()
-ui = Ui_MainWindow()
-ui.setupUi(window)
+# window = QtWidgets.QMainWindow()
+ui = MainWindow()
+# ui.resize(800, 524)
 ui.pushButton_2.clicked.connect(QtWidgets.qApp.quit)
-window.show()
+ui.show()
 sys.exit(app.exec_())
