3ea3b5ae50065e427c39408c78649364970ed23d
Naumenko-KM <Naumenko-KM@yandex.ru>
2022-07-06 16:51:59 +0300

    Fix bugs

--- gans.py
+++ gans.py
@@ -14,8 +14,7 @@ def generate_image(path: str) -> np.array:
     Open image, feed it to generator.
     Return generated image in numpy array [height, width, 3]
     """
-    with Image.open(path) as im:
-        img_0 = im.convert('RGB')
+    img_0 = Image.open(path)
     img_0_batch, img_size = img_to_batch(img_0)
     img_1_batch = generate_batch(img_0_batch)
     img_1 = img_from_batch(img_1_batch, img_size)
@@ -25,7 +24,7 @@ def generate_image(path: str) -> np.array:
 def img_to_batch(img: Image.Image) -> np.array:
     """
     Return batch of image's crops and image size
-    [height, width, 3] -> [B, 3, 256, 256]
+    [height, width, 3] -> [B, 256, 256, 3]
     """
     batch = []
     for x in range(0, img.size[0]-256, 256):
@@ -33,18 +32,14 @@ def img_to_batch(img: Image.Image) -> np.array:
             batch.append(np.array(img.crop((x, y, x+256, y+256))))
             # [256, 256, 3]
     batch = np.array(batch)  # [B, 256, 256, 3]
-    batch = np.swapaxes(batch, 3, 1)
-    batch = np.swapaxes(batch, 3, 2)
     return batch, (img.size[0]//256, img.size[1]//256)
-    # return np.ones((4, 3, 256, 256))
+    # return np.ones((4, 256, 256, 3))
 
 
 def img_from_batch(batch: np.array, img_size: tuple) -> np.array:
     """
     Return image from batch of crops
     """
-    batch = np.swapaxes(batch, 3, 2)
-    batch = np.swapaxes(batch, 3, 1)
     batch = batch.reshape((img_size[0], img_size[1], batch.shape[1],
                            batch.shape[2], batch.shape[3]))
     stacked = stack_batch_v(batch[0], img_size[1])
@@ -71,7 +66,9 @@ def generate_batch(batch):
     model.load_state_dict(checkpoint["state_dict"])
     model.eval()
     batch = torch.Tensor(batch).to(DEVICE)
+    batch = batch.permute(0, 3, 1, 2)
     y_fake = model(batch)
+    y_fake = y_fake.permute(0, 2, 3, 1)
     y_fake = y_fake.detach().cpu().numpy()
     return y_fake
 
