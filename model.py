bb161d695e2593a8fa4ff3f5e856a8d56e41b495
Naumenko-KM <Naumenko-KM@yandex.ru>
2022-07-19 12:31:42 +0300

    PEP8

--- model.py
+++ model.py
@@ -21,9 +21,9 @@ def generate_image(path: str, mode: int) -> np.array:
     if mode == 0:
         img_1_cropped = generate_image_vis2inf(img_0_cropped)
     elif mode == 1:
-        img_1_cropped = generate_image_sum2win(img_0_cropped)
-    elif mode == 2:
         img_1_cropped = generate_image_win2sum(img_0_cropped)
+    elif mode == 2:
+        img_1_cropped = generate_image_sum2win(img_0_cropped)
     img_1 = uncrop_image(img_1_cropped, img_size)
     return img_1
 
@@ -40,13 +40,12 @@ def crop_image(img: Image.Image) -> tuple((np.array, tuple)):
             # [256, 256, 3]
     batch = np.array(batch)  # [B, 256, 256, 3]
     return batch, (img.size[0]//256, img.size[1]//256)
-    # return np.ones((4, 256, 256, 3))
 
 
 def uncrop_image(batch: np.array, img_size: tuple) -> np.array:
     """
     Return image from batch of crops
-    [B, 256, 256, 3], (height//256, width//256) -> [height, width, 3] 
+    [B, 256, 256, 3] -> [height, width, 3]
     """
     batch = batch.reshape((img_size[0], img_size[1], batch.shape[1],
                            batch.shape[2], batch.shape[3]))
@@ -64,7 +63,7 @@ def stack_batch_v(h_batch, size):
     return v_stacked
 
 
-def generate_image_vis2inf(batch, batch_size=64) -> np.array: 
+def generate_image_vis2inf(batch, batch_size=64) -> np.array:
     """
     Return generated cropped image
     """
@@ -73,9 +72,11 @@ def generate_image_vis2inf(batch, batch_size=64) -> np.array:
     checkpoint = torch.load('gen.pth', map_location=DEVICE)
     model.load_state_dict(checkpoint["state_dict"])
     model.eval()
+
     batch = batch / 255
     batch = torch.Tensor(batch).to(DEVICE)
     batch = batch.permute(0, 3, 1, 2)
+
     with torch.no_grad():
         y_fake = model(batch[:batch_size])
         for i in range(1, batch.shape[0]//batch_size + 1):
@@ -85,17 +86,16 @@ def generate_image_vis2inf(batch, batch_size=64) -> np.array:
 
     y_fake = y_fake.permute(0, 2, 3, 1)
     y_fake = y_fake.detach().cpu().numpy()
-    # print(y_fake)
     y_fake = (y_fake * 255).astype(np.uint8)
     y_fake[:, :, :, 1:] = 0
     return y_fake
 
 
-def generate_image_sum2win(batch, batch_size=64) -> np.array: 
+def generate_image_sum2win(batch, batch_size=64) -> np.array:
     pass
 
 
-def generate_image_win2sum(batch, batch_size=64) -> np.array: 
+def generate_image_win2sum(batch, batch_size=64) -> np.array:
     pass
 
 
